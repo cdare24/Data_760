feb018b0d253c1506a00b878794131bb8cc6ddeb
Claudia Dare <cdare@ad.unc.edu>
2025-07-22 23:50:44 -0400

    initial commit with interactive map

---
+++ main.js
@@ -0,0 +1,12 @@
+var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+    height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+var svg = d3.select("body")
+    .append("svg")
+    .style("cursor", "move");
+
+svg.attr("viewBox", "50 10" + width + " " + height)
+    .attr('preserveAspectRatio', 'xMinYMin');
+
+var map = svg.append('g')
+    .attr("class", "map");
---
+++ making_data_ks.Rmd.R
@@ -0,0 +1,2 @@
+library(readr)
+data <- read_csv('uswtdb_V8_1_20250522.csv')
